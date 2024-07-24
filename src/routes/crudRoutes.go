2c81f15ce120a93d21c2ede59b410a1671d3b93c
unknown <f20201815@pilani.bits-pilani.ac.in>
2024-07-24 19:14:25 +0530

    Migrated router from gorilla/mux to http/ServeMux

--- src/routes/crudRoutes.go
+++ src/routes/crudRoutes.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	"GoSQLReactProject/src/controllers"
-	"github.com/gorilla/mux"
+	"net/http"
 )
 
-var FoodAppRoutes = func(router *mux.Router) {
-	router.HandleFunc("/createDish", controllers.CreateDish).Methods("POST")
-	router.HandleFunc("/getDish/", controllers.GetAllDish).Methods("GET")
-	router.HandleFunc("/updateDish/:id", controllers.UpdateDish).Methods("PUT")
-	router.HandleFunc("/deleteDish/:id", controllers.DeleteDish).Methods("DELETE")
-	router.HandleFunc("/getDish/:id", controllers.GetDish).Methods("GET")
+var FoodAppRoutes = func(router http.ServeMux) {
+	router.HandleFunc("PUT /createDish", controllers.CreateDish)
+	router.HandleFunc("GET /getDish/", controllers.GetAllDish)
+	router.HandleFunc("GET /updateDish/:id", controllers.UpdateDish)
+	router.HandleFunc("DELETE /deleteDish/:id", controllers.DeleteDish)
+	router.HandleFunc("GET /getDish/:id", controllers.GetDish)
 }
