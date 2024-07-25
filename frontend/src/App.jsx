433185c6d37dd43fbe7d80c4ba6317adfe91f7e9
unknown <f20201815@pilani.bits-pilani.ac.in>
2024-07-25 18:08:08 +0530

    Added Header, overall style for WebApp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.scss'
+import Home from './components/Home'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import detail from './components/List/Dish'
+import errorpage from './components/Error/Errorpage'
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+   <div className='App'>
+    <Router>
+      <Header></Header>
+      <div className='container'>
+      <Routes>
+        <Route path='/' Component={Home}></Route>
+      <Route path='/movie/:imdbID' Component={detail}></Route>
+      <Route Component={errorpage}></Route>
+      </Routes>
+      </div>
+      <Footer></Footer>
+    </Router>
+   </div>
   )
 }
 
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import './Header.scss'
+import wine from '../../assets/wine.png'
+
+const header = () => {
+    return (
+        <div className='header'>
+            <Link to= "/" className='Link'>
+                <div className='logo'>Bistro Sicilliana </div>
+            </Link>
+            <div className='wine-image'>
+                <img src= {wine} alt='wine'></img>
+            </div>
+        </div>
+    )
+};
+
+export default header;
