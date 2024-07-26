0305f2b907b2bf586298bc09cfedd2d1065c14b3
unknown <f20201815@pilani.bits-pilani.ac.in>
2024-07-26 12:26:58 +0530

    Enable CORS in app.go

--- app.go
+++ app.go
@@ -10,9 +10,25 @@ import (
 
 func main() {
 	router := http.NewServeMux()
+	handler := enableCORS(router)
+	http.Handle("/", handler)
 	util.Connect()
 	models.Init()
 	routes.FoodAppRoutes(router)
-	http.Handle("/", router)
-	log.Fatal(http.ListenAndServe("localhost:8080", router))
+	// Enable CORS
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+// enableCORS enables Cross-Origin Resource Sharing (CORS) for the given handler.
+func enableCORS(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "content-Type, content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		log.Printf("CORS is enabled!")
+		w.WriteHeader(http.StatusOK)
+		handler.ServeHTTP(w, r)
+	})
 }
